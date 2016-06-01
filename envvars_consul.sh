b0b32a21f42f75f10b369724c41e973f9d6df2f4
mugdho <mdebnath@glynlyon.com>
2016-06-01 09:36:56 -0700

    Updating consul script and removing the python script

--- envvars_consul.sh
+++ envvars_consul.sh
@@ -2,20 +2,26 @@
 
 # This script expects the following ENV variables to be setup
 # CONSUL_URL, APP, APP_ENV
+# If no CONSUL_URL this script will assume that all the env variables are set
+# through command line. We can tool this behavior as needed
 
-BASE_URL="${CONSUL_URL}/v1/kv"
-BASE_KEY="${APP}/${APP_ENV}"
-APP_URL="${BASE_URL}/${BASE_KEY}"
-KEYS_URL="${APP_URL}/?keys"
+if [ "$CONSUL_URL" != "" ]
+then
 
-KEYS=( $( curl $KEYS_URL 2> /dev/null| jq --raw-output ".[]" ) )
+  BASE_URL="${CONSUL_URL}/v1/kv"
+  BASE_KEY="${APP}/${APP_ENV}"
+  APP_URL="${BASE_URL}/${BASE_KEY}"
+  KEYS_URL="${APP_URL}/?keys"
 
-for KEY in "${KEYS[@]}"
-do
-  if [ $KEY != "${BASE_KEY}/" ]
-  then
-    VALUE=$( curl "$BASE_URL/${KEY}?raw" 2> /dev/null )
-    KEY=${KEY/#$BASE_KEY\//}
-    export "$KEY"="$VALUE"
-  fi
-done
+  KEYS=( $( curl $KEYS_URL 2> /dev/null| jq --raw-output ".[]" ) )
+
+  for KEY in "${KEYS[@]}"
+  do
+    if [ $KEY != "${BASE_KEY}/" ]
+    then
+      VALUE=$( curl "$BASE_URL/${KEY}?raw" 2> /dev/null )
+      KEY=${KEY/#$BASE_KEY\//}
+      export "$KEY"="$VALUE"
+    fi
+  done
+fi
