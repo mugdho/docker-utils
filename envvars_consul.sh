6bb18aa5d513247cfbb6336f9746363ba8c4c51a
mugdho <mdebnath@glynlyon.com>
2016-08-16 10:31:15 -0700

    Added support for secured consul servers

--- envvars_consul.sh
+++ envvars_consul.sh
@@ -5,13 +5,20 @@
 # If no CONSUL_URL this script will assume that all the env variables are set
 # through command line. We can tool this behavior as needed
 
+# Set the CONSUL_TOKEN variable to default if one not provided. This means no acls have been set up.
+# Always provide the READ ONLY ACL token here. NEVER the master token for consul.
+if [ "$CONSUL_TOKEN" == "" ]
+then
+    CONSUL_TOKEN="default-token"
+fi
+
 if [ "$CONSUL_URL" != "" ]
 then
 
   BASE_URL="${CONSUL_URL}/v1/kv"
   BASE_KEY="${APP}/${APP_ENV}"
   APP_URL="${BASE_URL}/${BASE_KEY}"
-  KEYS_URL="${APP_URL}/?keys"
+  KEYS_URL="${APP_URL}/?keys&token=$CONSUL_TOKEN"
 
   KEYS=( $( curl $KEYS_URL 2> /dev/null| jq --raw-output ".[]" ) )
 
@@ -19,7 +26,7 @@ then
   do
     if [ $KEY != "${BASE_KEY}/" ]
     then
-      VALUE=$( curl "$BASE_URL/${KEY}?raw" 2> /dev/null )
+      VALUE=$( curl "$BASE_URL/${KEY}?raw&token=$CONSUL_TOKEN" 2> /dev/null )
       KEY=${KEY/#$BASE_KEY\//}
       export "$KEY"="$VALUE"
     fi
